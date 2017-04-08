b1bed94990aa5fbe956ebc2d4e87e993af65186f
HillGiant <mathew.luebbert@gmail.com>
2017-04-08 12:15:42 -0700

    improvements made last year. Ignoring old config file

--- ClimbFinder.py
+++ ClimbFinder.py
@@ -1,7 +1,6 @@
 import json
 import os.path
 import urllib.request
-import parsedatetime as pdt
 import re
 import time
 import smtplib
@@ -9,6 +8,7 @@ from time import mktime
 from datetime import datetime
 from bs4 import BeautifulSoup
 from email.mime.text import MIMEText
+from dateutil.parser import parse
 
 print("Hello World")
 
@@ -34,7 +34,11 @@ class ActivityRaw:
             self.leader_availability = " ".join(availability_spans[1].stripped_strings)
         else:
             self.leader_availability = ""
-        self.registration_info = activity_div.find(class_="result-reg").string
+        registration_info_div = activity_div.find(class_="result-reg")
+        if(registration_info_div == None):
+            self.registration_info = ""
+        else:
+            self.registration_info = registration_info_div.string
         difficulty_div = activity_div.find(class_="result-difficulty")
         if(difficulty_div == None):
             self.difficulty = ""
@@ -67,17 +71,16 @@ class ActivityRaw:
 
 class Activity:
     def __init__(self, activity_raw):
-        p = pdt.Calendar()
         digits_re = re.compile(r"\d+")
         self.type = activity_raw.type
         self.name = activity_raw.name
-        self.start_date = datetime.fromtimestamp(mktime(p.parse(activity_raw.start_date)[0]))
-        self.end_date = datetime.fromtimestamp(mktime(p.parse(activity_raw.end_date)[0])) if activity_raw.end_date else None
+        self.start_date = parse(activity_raw.start_date)
+        self.end_date = parse(activity_raw.end_date) if activity_raw.end_date else None
         self.participant_availability = int(digits_re.match(activity_raw.participant_availability).group(0) if activity_raw.participant_availability else 0)
         self.participant_availability = -1 * self.participant_availability if "waitlist" in activity_raw.participant_availability else self.participant_availability
         self.leader_availability = int(digits_re.match(activity_raw.leader_availability).group(0) if activity_raw.leader_availability else 0)
         self.leader_availability = -1 * self.leader_availability if "waitlist" in activity_raw.leader_availability else self.leader_availability
-        self.registration_info = activity_raw.registration_info
+        self.registration_info = activity_raw.registration_info if activity_raw.registration_info else None
         if("opens" in activity_raw.registration_info):
             self.registration_status = "Not yet open"
         elif("closed" in activity_raw.registration_info):
@@ -91,6 +94,9 @@ class Activity:
         self.number_of_days = (self.end_date - self.start_date).days + 1 if self.end_date else 1
 
     def to_string(self):
+        status = self.registration_status
+        if(self.registration_info):
+            status = status + " (" + self.registration_info + ")"
         return "\n".join(
             ("Type: -------------- " + self.type,
              "Name:                " + self.name,
@@ -99,7 +105,7 @@ class Activity:
              "Days: -------------- " + str(self.number_of_days),
              "Availability:        " + str(self.participant_availability),
              "Leader Availability: " + str(self.leader_availability),
-             "Status:              " + self.registration_status + " (" + self.registration_info + ")",
+             "Status:              " + status,
              "Difficulty: -------- " + self.difficulty,
              "Prereqs:             " + self.prereqs,
              "Branch: ------------ " + self.branch,
@@ -107,6 +113,9 @@ class Activity:
              ))
     
     def to_email_string(self):
+        status = self.registration_status
+        if(self.registration_info):
+            status = status + " (" + self.registration_info + ")"
         return "\n".join(
             (
              "<div>",
@@ -117,7 +126,7 @@ class Activity:
              "<tr><td>{0}</td><td>{1}</td></tr>".format("End Date", (self.end_date.strftime("%a, %b %d, %y") if self.end_date else "")),
              "<tr><td>{0}</td><td>{1}</td></tr>".format("Days", self.number_of_days),
              "<tr><td>{0}</td><td>{1}</td></tr>".format("Availability", str(self.participant_availability) + " open spots"),
-             "<tr><td>{0}</td><td>{1}</td></tr>".format("Status", self.registration_status + " (" + self.registration_info + ")"),
+             "<tr><td>{0}</td><td>{1}</td></tr>".format("Status", status),
              "<tr><td>{0}</td><td>{1}</td></tr>".format("Branch", self.branch),
              "</table>",
              "</div>",
@@ -135,9 +144,27 @@ class RuleFactory:
 
 class BasicClimbingRule:
     def match(self, activity):
-        return activity.type == "Basic Alpine Climb" and activity.participant_availability > 0 and activity.registration_status != "Closed"
+        return (activity.type == "Basic Alpine Climb" or activity.type == "Glacier Climb") and activity.participant_availability > 0 and activity.registration_status != "Closed"
 		
-    rule_name = "BasicClimbingRule"
+    rule_name = "Basic Climb"
+
+class BasicClimbingLeaderRule:
+    def match(self, activity):
+        return (activity.type == "Basic Alpine Climb" or activity.type == "Glacier Climb") and activity.leader_availability > 0 and activity.registration_status != "Closed"
+		
+    rule_name = "Basic Climb Leader"
+
+class IntermediateClimbingRule:
+    def match(self, activity):
+        return (activity.type == "Intermediate Alpine Climb") and activity.participant_availability > 0 and activity.registration_status != "Closed"
+		
+    rule_name = "Intermediate Climb"
+
+class ScramblingRule:
+    def match(self, activity):
+        return (activity.type == "Alpine Scramble") and activity.participant_availability > 0 and activity.registration_status != "Closed"
+		
+    rule_name = "Alpine Scramble"
 
 class EasyKayakingRule:
     def match(self, activity):
@@ -153,7 +180,7 @@ class EasyKayakingRule:
 			activity.registration_status != "Closed"
                 )
 		
-    rule_name = "EasyKayakingRule"
+    rule_name = "Kayaking"
 
 def getAllActivities():
     activities = []
@@ -185,43 +212,72 @@ def sendEmail(content, targets, config):
 	server.sendmail(fromaddr, targets, msg.as_string())
 	server.quit()
 
-def find_activities(config, rule_factory):
+def execute(config, rule_factory):
     activities = getAllActivities()
+    rule_to_activities = build_rule_to_activities(activities, [rule["rule_name"] for rule in config["rules"]])
+    for name, rules in name_to_rules.items():
+        has_content = False;
+        email_body = ""
+        for rule in rules:
+            if(len(rule_to_activities[rule["rule_name"]]) != 0):
+                has_content = True
+                email_body = email_body + "<h1>" + rule["rule_name"] + "</h1>" + "\n<hr \>\n".join([activity.to_email_string() for activity in rule_to_activities[rule["rule_name"]]])
 
-    for rule_group in config["rules"]:
-        rule = rule_factory.get_rule(rule_group["rule_name"])
-        file_name = rule.rule_name + ".cache"
-        if os.path.isfile(file_name):
-            infile = open(file_name, 'r')
-            seen_links = infile.read().splitlines()
-            infile.close()
-        else:
-            seen_links = []
+        if(has_content):
+            print("sending email to {0}".format(name))
+            sendEmail(config["email_template"].format(email_body), [name], config)
 
-        print(seen_links)
+def build_rule_to_activities(activities, rule_names):
+    rule_to_activites = {}
+    for rule_name in rule_names:
+        rule = rule_factory.get_rule(rule_name)
+        seen_links = get_seen_links(rule_name)
+
         filtered_activities = [activity for activity in activities if rule.match(activity)]
+        set_seen_links(rule.rule_name, filtered_activities)
 
         unseen_activities = [activity for activity in filtered_activities if not activity.link in seen_links]
+        rule_to_activites[rule_name] = unseen_activities
+        print("For {3}: {0} activities total. {1} met the criteria. {2} have not been seen before".format(len(activities), len(filtered_activities), len(unseen_activities), rule_name))
+    return rule_to_activites
 
-        outfile = open(file_name, 'w')
-        for link in [activity.link for activity in filtered_activities]:
-            print(link)
-            outfile.write("{0}\n".format(link))
-        outfile.close()
 
-        print("{0} activities total. {1} met the criteria. {2} have not been seen before".format(len(activities), len(filtered_activities), len(unseen_activities)))
-        if(len(unseen_activities) != 0):
-            sendEmail(config["email_template"].format("\n<hr \>\n".join([activity.to_email_string() for activity in unseen_activities])), rule_group["distribution_list"], config)
+def get_seen_links(rule_name):
+    file_name = rule_name + ".cache"
+    seen_links = []
+    if os.path.isfile(file_name):
+        infile = open(file_name, 'r')
+        seen_links = infile.read().splitlines()
+        infile.close()
+    return seen_links
+
+def set_seen_links(rule_name, filtered_activities):
+    file_name = rule_name + ".cache"
+    outfile = open(file_name, 'w')
+    for link in [activity.link for activity in filtered_activities]:
+        outfile.write("{0}\n".format(link))
+    outfile.close()
+        
 
 configFile = open(CONFIG_FILE, 'r')
 config = json.load(configFile)
+name_to_rules = {}
+for rule_group in config["rules"]:
+    for email in rule_group["distribution_list"]:
+        if not email in name_to_rules:
+            name_to_rules[email] = []
+        name_to_rules[email].append(rule_group)
+
 rule_factory = RuleFactory()
 rule_factory.register(BasicClimbingRule())
+rule_factory.register(BasicClimbingLeaderRule())
+rule_factory.register(IntermediateClimbingRule())
+rule_factory.register(ScramblingRule())
 rule_factory.register(EasyKayakingRule())
 print(config["email_username"])                           
 print(config["email_template"])                          
 print(config["email_subject"])
 
 while True:
-    find_activities(config, rule_factory)
+    execute(config, rule_factory)
     time.sleep(3600)
